be87ccbc594b6ce72e7304e27637abd690f80d7a
Nick Pavlov <pavlov.nikk@gmail.com>
2015-07-17 16:41:48 +0300

    commit

--- src/main/groovy/com/sysgears/Service.groovy
+++ src/main/groovy/com/sysgears/Service.groovy
@@ -40,29 +40,20 @@ class Service {
         Map data = (Map) jsonParser.get("currently")
 
         println "\n\nCurrently\n${jsonParser.get("currently")}\n"
-        println "Summary: ${data.get("summary")}"
-        println "Temperature: ${data.get("temperature")}"
+        //println "Summary: ${data.get("summary")}"
+        //println "Temperature: ${data.get("temperature")}"
         println "WindSpeed: ${data.get("windSpeed")}"
-        println "humidity: ${data.get("humidity")}"
+        //println "humidity: ${data.get("humidity")}"
 
         String url = "https://push.geckoboard.com/v1/send/"
-
         String windSpeedWidget = "152712-5d5248f3-f78a-4f2d-b018-546aee106155"
-        String temperatureWidget = "152712-ecb527c2-540e-4894-97db-23f5cc6f1d9b"
-
-
-        String temperatureBody = "{\"api_key\":\"c143d855c29d5fe59d2ce0830c834e04\"," +
-                "\"data\":{\"item\":${data.get("temperature")},\"min\":{\"value\":0},\"max\":{\"value\":100}}}"
 
         String windSpeedBody = "{\"api_key\":\"c143d855c29d5fe59d2ce0830c834e04\"," +
                 "\"data\":{\"item\":${data.get("windSpeed")},\"min\":{\"value\":0},\"max\":{\"value\":100}}}"
 
-
-
         Map<String, String> headers = new HashMap<String, String>()
         headers.put("Content-Type", "application/json")
 
-        Http.post(url + temperatureWidget, headers, temperatureBody)
         Http.post(url + windSpeedWidget, headers, windSpeedBody)
 
         /*
